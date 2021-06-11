e7d8b70cb5c08a7f047ca220cfe424ff3a4a21f6
Guilherme <parra.guioliveira@hotmail.com>
2021-06-10 22:22:48 -0300

    Historico finalizado

--- pages/historico/index.js
+++ pages/historico/index.js
@@ -1,7 +1,145 @@
 import React  from "react";
-import { StyleSheet, View, Text, TextInput, FlatList, SafeAreaView} from 'react-native';
+import { StyleSheet, View, Text, TextInput, FlatList, SafeAreaView, ScrollView} from 'react-native';
+import {
+  PieChart,
+  StackedBarChart
+} from "react-native-chart-kit";
 
 import Cabecalho from "../../components/cabecalho";
+
+/*const Graphic = (
+  <View>
+  <Text>Bezier Line Chart</Text>
+  <LineChart
+    data={{
+      labels: ["January", "February", "March", "April", "May", "June"],
+      datasets: [
+        {
+          data: [
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100,
+            Math.random() * 100
+          ]
+        }
+      ]
+    }}
+  width={/*Dimensions.get("window").width 250} // from react-native
+    height={220}
+    yAxisLabel="$"
+    yAxisSuffix="k"
+    yAxisInterval={1} // optional, defaults to 1
+    chartConfig={{
+      backgroundColor: "#e26a00",
+      backgroundGradientFrom: "#fb8c00",
+      backgroundGradientTo: "#ffa726",
+      decimalPlaces: 2, // optional, defaults to 2dp
+      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+      style: {
+        borderRadius: 16
+      },
+      propsForDots: {
+        r: "6",
+        strokeWidth: "2",
+        stroke: "#ffa726"
+      }
+    }}
+    bezier
+    style={{
+      marginVertical: 8,
+      borderRadius: 16
+    }}
+  />
+</View>
+)*/
+
+
+const Graphic=()=>{
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      
+        <View style={styles.container}>
+          <View>
+            {/*Example of Pie Chart*/}
+            <MyPieChart />
+          </View>
+        </View>
+      
+    </SafeAreaView>
+  );
+};
+const MyPieChart = () => {
+  return (
+    <>
+      <Text style={styles.header}>Histórico de Entregas</Text>
+      <PieChart
+        data={[
+          {
+            name: 'Entregas',
+            population: 10,
+            color: '#2ECC71',
+            legendFontColor: '#7F7F7F',
+            legendFontSize: 15,
+          },
+          {
+            name: 'Ocorrências',
+            population: 5,
+            color: '#F29035',
+            legendFontColor: '#7F7F7F',
+            legendFontSize: 15,
+          },
+          {
+            name: 'Devoluções',
+            population: 8,
+            color: '#E92525',
+            legendFontColor: '#7F7F7F',
+            legendFontSize: 15,
+          }
+        ]}
+      width={300/*Dimensions.get('window').width - 16*/}
+        height={200}
+        chartConfig={{
+          backgroundColor: '#1cc910',
+          backgroundGradientFrom: '#eff3ff',
+          backgroundGradientTo: '#efefef',
+          decimalPlaces: 2,
+          color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+          style: {
+            borderRadius: 16,
+          },
+        }}
+        style={{
+          marginVertical: 8,
+          borderRadius: 16,
+        }}
+        accessor="population"
+        backgroundColor="transparent"
+        paddingLeft="15"
+        absolute //for the absolute number remove if you want percentage
+      />
+    </>
+  );
+};
+/*
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    padding: 10,
+  },
+  header: {
+    textAlign: 'center',
+    fontSize: 18,
+    padding: 16,
+    marginTop: 16,
+  },
+});*/
 
 const DATA = [
     {
@@ -45,6 +183,8 @@ const Historico =()=>{
 
             <Cabecalho/> 
 
+            
+
             {/* linha verde do cabecalho */}
             <View style={{width: '100%', height: '0.5%', backgroundColor: '#2ECC71'}}></View> 
 
@@ -52,7 +192,8 @@ const Historico =()=>{
 
             {/* linha para a separação das seções (acima ficara o grafico)
             <View style={{width: '85%', height: '0,4%', backgroundColor: '#C8C8C8'}}></View> */}
-
+            <Graphic/>
+            
             <Text>Pesquisar nota</Text>
 
             <View style={styles.input}>
@@ -79,6 +220,20 @@ const Historico =()=>{
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    padding: 10,
+  },
+  header: {
+    textAlign: 'center',
+    fontSize: 18,
+    padding: 16,
+    marginTop: 16,
+  },
         container: {
         flex: 1,
         backgroundColor: '#F7F7F7',
@@ -117,8 +272,17 @@ const styles = StyleSheet.create({
         title: {
             fontSize: 20,
               },
-           
-          
+  
 });
+const chartConfig = {
+  backgroundGradientFrom: "#1E2923",
+  backgroundGradientFromOpacity: 0,
+  backgroundGradientTo: "#08130D",
+  backgroundGradientToOpacity: 0.5,
+  color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+  strokeWidth: 2, // optional, default 3
+  //barPercentage: 0.5,
+  useShadowColorFromDataset: false // optional
+};
 
 export default Historico;
